89e42b2c8161d91fe27f61a3070e061e340915bd
Ben Brunton <ben.brunton@infinityworks.com>
2018-03-07 14:43:53 +0000

    100% coverage

--- test/storeIdsMiddleware.test.js
+++ test/storeIdsMiddleware.test.js
@@ -1,8 +1,27 @@
+const { createNamespace } = require('cls-hooked');
 const createStoreIdsMiddleware = require('../src/storeIdsMiddleware');
 const assert = require('assert');
+const sinon = require('sinon');
 
 describe('storeIdsMiddleware', () => {
-    it('exists', () => {
-        assert(typeof createStoreIdsMiddleware !== 'undefined');
+    const namespace = 'test-ns';
+    let storeIdsMiddleware;
+    let requestNamespace;
+    let req;
+    let res;
+    let next;
+
+    beforeEach(() => {
+        req = {};
+        res = {};
+        next = () => {};
+        requestNamespace = createNamespace(namespace);
+        storeIdsMiddleware = createStoreIdsMiddleware(namespace);    
+    });
+
+    it('calls next', () => {
+        const nextSpy = sinon.spy();
+        storeIdsMiddleware(req, res, nextSpy);
+        assert(nextSpy.called);
     });
 });
