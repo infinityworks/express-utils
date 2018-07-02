e3d06c7f92dbf652449d9f68da94c0954a25a3a9
bianca linscott <bianca.linscott@infinityworks.com>
2018-07-02 11:40:54 +0100

    [GB-375] add user agent to logs

--- src/storeIdsMiddleware.js
+++ src/storeIdsMiddleware.js
@@ -9,6 +9,7 @@ module.exports = (namespace) => {
             req.uuid = uuid.v1();
             requestNamespace.set('reqId', req.uuid);
             requestNamespace.set('sessionId', req.sessionID);
+            requestNamespace.set('userAgent', req.headers['user-agent']);
             next();
         });
     };
